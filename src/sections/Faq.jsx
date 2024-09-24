f4adc23fba78ee43c8668ad4eba5fa89d4a3f034
Jonatandb <jonatandb@gmail.com>
2024-09-24 18:16:21 -0300

    Fix wrong div closing tag

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -19,30 +19,26 @@ const Faq = () => {
           </div>
 
           <div className='faq-line_after w-0.5 h-full absolute left-[calc(50%-1px)] top-0 -z-1 bg-s2' />
+        </div>
 
-          <div className='faq-glow_before relative z-2 border-2 border-s2 bg-s1'>
-            <div className='container flex gap-10 max-lg:block'>
-              <div className='rounded-half absolute -top-10 left-[calc(50%-40px)] z-4 flex size-20 items-center justify-center border-2 border-s2 bg-s1'>
-                <img
-                  src='/images/faq-logo.svg'
-                  alt='logo'
-                  className='size-1/2'
-                />
-              </div>
-              <div className='relative flex-1 pt-24'>
-                {faqList.slice(0, halfLength).map((faq, idx) => (
-                  <FaqItem key={faq.id} item={faq} index={idx} />
-                ))}
-              </div>
-              <div className='relative flex-1 lg:pt-24'>
-                {faqList.slice(halfLength).map((faq, idx) => (
-                  <FaqItem key={faq.id} item={faq} index={halfLength + idx} />
-                ))}
-              </div>
+        <div className='faq-glow_before relative z-2 border-2 border-s2 bg-s1'>
+          <div className='container flex gap-10 max-lg:block'>
+            <div className='rounded-half absolute -top-10 left-[calc(50%-40px)] z-4 flex size-20 items-center justify-center border-2 border-s2 bg-s1'>
+              <img src='/images/faq-logo.svg' alt='logo' className='size-1/2' />
             </div>
-
-            <div className='faq-line_after absolute left-[calc(50%-1px)] top-0 -z-1 h-full w-0.5 bg-s2 max-lg:hidden'></div>
+            <div className='relative flex-1 pt-24'>
+              {faqList.slice(0, halfLength).map((faq, idx) => (
+                <FaqItem key={faq.id} item={faq} index={idx} />
+              ))}
+            </div>
+            <div className='relative flex-1 lg:pt-24'>
+              {faqList.slice(halfLength).map((faq, idx) => (
+                <FaqItem key={faq.id} item={faq} index={halfLength + idx} />
+              ))}
+            </div>
           </div>
+
+          <div className='faq-line_after absolute left-[calc(50%-1px)] top-0 -z-1 h-full w-0.5 bg-s2 max-lg:hidden'></div>
         </div>
       </Element>
     </section>
